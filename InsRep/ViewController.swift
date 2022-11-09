75d3679fd55daf3a8e7a130a48dfbf204e10aca9
oguzhan <oguzhan@vivoo.me>
2022-11-09 14:44:21 +0300

    Initial Commit

---
+++ InsRep/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InsRep
+//
+//  Created by Oğuzhan Kabul on 9.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InsRepUITests/InsRepUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  InsRepUITestsLaunchTests.swift
+//  InsRepUITests
+//
+//  Created by Oğuzhan Kabul on 9.11.2022.
+//
+
+import XCTest
+
+class InsRepUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
